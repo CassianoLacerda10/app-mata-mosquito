fb867bb82c43d8bf1e722b659302fa712547a911
Cassiano <cassianolacerda10@gmail.com>
2022-11-01 22:38:16 -0300

    First commit

---
+++ jogo.js
@@ -0,0 +1,117 @@
+var altura = 0;
+var largura = 0;
+var vidas = 1;
+var tempo = 15;
+
+var criaMoscaTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+	criaMoscaTempo = 1500
+} else if (nivel === 'dificil') {
+	criaMoscaTempo = 1000
+} else if (nivel === 'chucknorris') {
+	criaMoscaTempo = 750
+}
+
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	//console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function () {
+
+	tempo--
+
+	if (tempo < 0 ) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+		window.location.href = 'vitoria.html'
+	}else {
+		document.getElementById('cronometro').innerHTML = tempo
+	}
+}, 1000)
+
+
+function posicaoRandomica() {
+
+	//remover o mosquito anterior (caso exista)
+	if (document.getElementById('mosca')) {
+		document.getElementById('mosca').remove()
+
+		//console.log('Elemento selecionado foi ' + vidas)
+
+		if(vidas > 3) {
+			window.location.href = 'fim_de_jogo.html'
+		} else {
+			document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+		}
+
+		vidas++
+	}
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//criar o elemento HTML
+	var mosca = document.createElement('img')
+	mosca.src = 'imagens/mosca.png'
+	mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+	mosca.style.left = posicaoX + 'px'
+	mosca.style.top = posicaoY + 'px'
+	mosca.style.position = 'absolute'
+	mosca.id = 'mosca'
+	mosca.onclick = function() {
+		this.remove()
+		var estralo = document.getElementById('estralo')
+		estralo.play()
+		estralo.volume = 0.2
+	}
+
+	var som_mosca = document.getElementById('som-mosca')
+	som_mosca.play()
+	som_mosca.volume = 0.2
+
+	document.body.appendChild(mosca)
+
+	
+	
+}
+
+function tamanhoAleatorio() {
+	var	classe = Math.floor(Math.random() * 3)
+
+	switch(classe) {
+		case 0:
+			return 'mosca1'
+
+		case 1:
+			return 'mosca2'
+
+		case 2:
+			return 'mosca3'
+	}
+}
+
+function ladoAleatorio() {
+	var lado = Math.floor(Math.random() * 2)
+
+	if (lado == 0) {
+		lado = 'ladoA'
+		return lado
+	} else {
+		lado = 'ladoB'
+		return lado
+	}
+}
